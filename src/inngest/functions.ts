eb7bbbdc298f49754d4508d94e21dc21ada5d28d
saksham-visionary <saksham@visionary.org.in>
2026-06-21 14:29:38 +0530

    inngest setup done

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,22 @@
 import { inngest } from "./client";
-
+import { Agent, createAgent, openai as agenticOpenAi } from "@inngest/agent-kit";
 export const helloWorld = inngest.createFunction(
   {
     id: "hello-world"
     , triggers: [{ event: "test/hello.world" }]
   },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "15s");
-    return { message: `Hello ${event.data.email}` };
+    const contentCreatorAgent = createAgent({
+      name: 'Content creator',
+      system:
+        'You are a expert summerizer , you are summerize in 10 words ',
+      model: agenticOpenAi({ model: "gpt-4o" }),
+    });
+    const { output } = await contentCreatorAgent.run(
+      "give me political opinion about youtube"
+    )
+    console.log(output);
+    //await step.sleep("wait-a-moment", "15s");
+    return { success: output };
   }
 );

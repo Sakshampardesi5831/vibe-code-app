33c477a5467eab998acab46d45f97f9c60aaf992
saksham-visionary <saksham@visionary.org.in>
2026-06-21 12:43:54 +0530

    trpc setup complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client'
 import { Button } from '@/components/ui/button'
 import React from 'react'
-
+import { useTRPC } from '@/trpc/client'
+// import { useQuery } from '@tanstack/react-query'
 const Home = () => {
+  const trpc = useTRPC();
+  trpc.hello.queryOptions({text:"Hello world"});
   return (
-    <div>Home
+    <div>
+      Home
       <Button>Click me</Button>
     </div>
   )

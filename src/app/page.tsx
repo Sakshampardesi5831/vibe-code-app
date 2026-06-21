f394d555236fbdb4adebd580973a60efc303d909
saksham-visionary <saksham@visionary.org.in>
2026-06-21 13:36:59 +0530

    inngest server setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,21 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import { useTRPC } from '@/trpc/client'
 // import { useQuery } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
 const Home = () => {
   const trpc = useTRPC();
-  trpc.hello.queryOptions({text:"Hello world"});
+  // trpc.hello.queryOptions({text:"Hello world"});
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  function clickMe() {
+    invoke.mutate({ text: "sakshampardesi5831@gmail.com" })
+  }
   return (
-    <div>
-      Home
-      <Button>Click me</Button>
+    <div
+      className='p-4 max-w-7xl mx-auto'
+    >
+      <Button
+        onClick={() => clickMe()}
+      >Click me</Button>
     </div>
   )
 }
